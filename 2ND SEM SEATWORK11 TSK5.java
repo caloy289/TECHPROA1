9bbf8a214299b78084c6a8920b79e478352928d3
caloy289 <abudjohncarlos28.scc@gmail.com>
2026-02-26 21:32:28 +0800

    Clean up comments in SeatworkElevenTaskFive
    Removed comments explaining Timer and TimerTask classes.

---
+++ 2ND SEM SEATWORK11 TSK5.java
@@ -0,0 +1,23 @@
+package SecondSemSeatworks;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SeatworkElevenTaskFive {
+    public static void main(String[] args) {
+
+      
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+
+            @Override
+            public void run() {
+                System.out.println("Hello!");
+            }
+        };
+
+        timer.schedule(task, 0, 1000); // (task, delay, period)
+
+    }
+}
